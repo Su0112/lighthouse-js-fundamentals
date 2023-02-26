2c6eb41cbdf6912aed00a990026564c0da7ad4d2
SuShruthi <shruthacm@gmail.com>
2023-02-25 23:14:31 -0800

    for..of loop example with arrays

--- looping-over-arrays.js
+++ looping-over-arrays.js
@@ -1,6 +1,14 @@
+//for loop
 const amounts = [61.0, 52.25, 112.99, 5.0];
 let total = 0;
 for (let i = 0; i < amounts.length; i++) {
   total += amounts[i];
 }
 console.log("Order total is: ", total);
+
+// for..of loop
+let total2 = 0;
+for (let amount of amounts) {
+  total2 += amount;
+}
+console.log("Order total is: ", total2);
