63f17b9dc838f5ba1399ff210e540312beaaffc9
SuShruthi <shruthacm@gmail.com>
2023-02-27 01:56:26 -0800

    to find if the number is even

---
+++ even.js
@@ -0,0 +1,13 @@
+// isEven function
+const isEven = function (num) {
+  return num % 2 === 0;
+};
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven); //true
+console.log(elevenIsEven); //false
+
+// console.log(isEven(10));
+// console.log(isEven(11));
