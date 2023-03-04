b66ec346aa0e5336928d736ba5d0985b48b470f5
SuShruthi <shruthacm@gmail.com>
2023-03-03 18:14:26 -0800

    completed the challenge for loops

--- ingredients.js
+++ ingredients.js
@@ -11,7 +11,20 @@ const ingredients = [
 ];
 
 // Write a while loop that prints out the contents of ingredients:
-
+console.log("Ingredients: ");
+let i = 0;
+while (i < ingredients.length) {
+  console.log(`${ingredients[i]}`);
+  i++;
+}
 // Write a for loop that prints out the contents of ingredients:
+console.log("Ingredients: ");
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(`${ingredients[i]}`);
+}
 
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
+console.log("Ingredients: ");
+for (let i = ingredients.length - 1; i > -1; i--) {
+  console.log(`${ingredients[i]}`);
+}
